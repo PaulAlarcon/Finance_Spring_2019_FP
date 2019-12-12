63f18f30f6b899fbe2bca0945fe1a04feee748ee
paul_alarcon <pa.f.e@hotmail.com>
2019-12-12 14:08:10 -0500

    .gitignore

--- src/Library.java
+++ src/Library.java
@@ -9,54 +9,72 @@ import static java.lang.Math.sqrt;
 
 final class Library {
 
-    Output binom(final Derivative deriv, final MarketData mkt, int n){
+    Output binom(final Derivative deriv, final MarketData mkt, int n) {
 
-        if(n < 1){
+        if (n < 1) {
             System.out.println("Invalid number n");
             return null;
         }
 
-        double deltaT = (deriv.T - mkt.t0)/n;
-        float u = (float) ( exp(mkt.sigma * sqrt(deltaT) ));
-        float d = 1/u;
-        float dk = (float) exp(mkt.r*deltaT);
-        float InvDk = (float) 1/dk;
+        double deltaT = (deriv.T - mkt.t0) / n;
+        double u = (exp(mkt.sigma * sqrt(deltaT)));
+        double d = 1 / u;
+        double dk =  exp(mkt.r * deltaT);
+        double InvDk =  1 / dk;
 
-        float p = (dk - d)/(u-d);
-        float q = 1 - p;
+        double p = (dk - d) / (u - d);
+        double q = 1 - p;
 
         Node root = new Node(mkt.Price, 0);
 
         Node tree[][] = new Node[n + 1][n + 1];
         //set root at first array, first pos
         tree[0][0] = root;
-        tree[1][0] = new Node(root.fairValue * d, 1  ); // sets
-        tree[1][1] = new Node(root.fairValue * u, 1 );
+        tree[1][0] = new Node(root.fairValue * d, 1); // sets
+        tree[1][1] = new Node(root.fairValue * u, 1);
         tree[2][1] = new Node(root.fairValue, 2);
 
+
+
+
+        //ArrayList instead of arr
+//        ArrayList<ArrayList<Node>> binom_tree = new ArrayList<ArrayList<Node>>();
+//        ArrayList<Integer>[] arr2 = new ArrayList[n + 1];
+
+//        binom_tree.get(0).add(root);
+//        binom_tree.get(1).add(new Node(root.fairValue*d, 1));
+//        binom_tree.get(1).add(new Node(root.fairValue*u, 1));
+//        binom_tree.get(2).add( 1, new Node(root.fairValue*u, 2) );
+
+
+//        int level = 2;
+//        for (int i = 2; i < n + 1; i++) {
+//            binom_tree.get(i).add(0, new Node(binom_tree.get(i-1).get(0).fairValue * d, i));
+//            binom_tree.get(i).add(level, new Node(binom_tree.get(i-1).get(level - 1).fairValue * d, i));
+//
+//            for (int j = 1; j < level; j++) {
+//                binom_tree.get(i).add(j, new Node( binom_tree.get(i -2 ).get(j - 1).fairValue, i));
+//            }
+//            level++;
+//        }
+//
+//        printBinomTree(binom_tree);
+
+
+        //Array Stuff
         int level = 2;
-        for(int i = 2; i < n + 1; i++){
+        for (int i = 2; i < n + 1; i++) {
             tree[i][0] = new Node(tree[i - 1][0].fairValue * d, i); // down
-            tree[i][level] = new Node( tree[i - 1][level - 1].fairValue * u, i); // up
-            for(int j = 1; j < level; j++){
-                    tree[i][j] = new Node(tree[i - 2][j - 1].fairValue, i);
+            tree[i][level] = new Node(tree[i - 1][level - 1].fairValue * u, i); // up
+            for (int j = 1; j < level; j++) {
+                tree[i][j] = new Node(tree[i - 2][j - 1].fairValue, i);
             }
             level++;
-
         }
 
-        // Printing
-        for(int i = 0; i < tree.length; i++){
-            for(int j = 0; j < tree[i].length; j++){
-                if(tree[i][j] != null){
-                    System.out.print( tree[i][j].fairValue + " ");
-                }
-                else {
-                    System.out.print(" ( ) ");
-                }
-            }
-            System.out.println();
-        }
+        printTree(tree);
+
+
 
 //        for(i = 1; i <= n + 1; i++){
 //            int firstIndex = 0;
@@ -91,10 +109,11 @@ final class Library {
 
         return null;
 
-    };
+    }
 
-    int impvol(final Derivative deriv, final MarketData mkt, int n , int max_iter , double tol , Output out){
-    // The function impvol executes a loop of iterations to calculate the implied volatility of a derivative.
+
+    int impvol(final Derivative deriv, final MarketData mkt, int n, int max_iter, double tol, Output out) {
+        // The function impvol executes a loop of iterations to calculate the implied volatility of a derivative.
 //        The market price of the derivative is supplied in theMarketDataobject.
 //        To calculate the implied volatility, the functionimpvolcallsbinomin a loop.
 //        The number of loop iterations and the implied volatility are returned in theOutputobject.
@@ -102,6 +121,35 @@ final class Library {
 
 
         return 1;
-    };
+    }
 
+    ;
+
+
+    static void printTree(Node[][] tree) {
+        // Printing
+        for (int i = 0; i < tree.length; i++) {
+            for (int j = 0; j < tree[i].length; j++) {
+                if (tree[i][j] != null) {
+                    System.out.print(tree[i][j].fairValue + " ");
+                } else {
+                    System.out.print(" ( ) ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void printBinomTree(ArrayList<ArrayList<Node>> tree) {
+        for (int i = 0; i < tree.size(); i++) {
+            for (int j = 0; j < tree.get(i).size(); j++) {
+//                if (tree[i][j] != null) {
+                    System.out.print(tree.get(i).get(j).fairValue + " ");
+//                } else {
+//                    System.out.print(" ( ) ");
+//                }
+            }
+            System.out.println();
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String args[]){
         Library l = new Library();
-        VanillaOption americanPut = new VanillaOption(0.4, VanillaOption.TYPE.PUT, VanillaOption.VERSION.AMERICAN);
-        MarketData mD = new MarketData(100, 100, 0.1, 0.5, 0);
+        VanillaOption americanPut = new VanillaOption(0.3, VanillaOption.TYPE.PUT, VanillaOption.VERSION.AMERICAN);
+        MarketData mD = new MarketData(100, 100, 0.1, 0.4, 0);
         int n = 4;
-        l.binom(americanPut, mD, 5);
+        l.binom(americanPut, mD, 3);
     }
 
 }
