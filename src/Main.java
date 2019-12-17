7492bea6defab11d4658272b4ec567ac9aeb863e
paul_alarcon <pa.f.e@hotmail.com>
2019-12-17 13:42:50 -0500

    .gitignore

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,13 @@ public class Main {
         Library l = new Library();
 //        VanillaOption americanCall = new VanillaOption(0.3, 100, VanillaOption.TYPE.CALL, VanillaOption.VERSION.AMERICAN);
         VanillaOption americanPut = new VanillaOption(0.4, 100, VanillaOption.TYPE.PUT, VanillaOption.VERSION.AMERICAN);
-        MarketData mD = new MarketData(100, 0.1, 0.05, 0); // for impliedvol we are not using sigma at all
+        MarketData mD = new MarketData(100, 0.1, 0.5, 0); // for impliedvol we are not using sigma at all
         System.out.println(l.binom(americanPut, mD, 4));
 
-        mD.setPrice(10.446); // you need fairValue at root;
-        Output out = new Output();
-        l.impvol(americanPut, mD, 4, 100, 000000.1, out );
-        System.out.println(out.impvol);
+//        mD.setPrice(10.446); // you need fairValue at root;
+//        Output out = new Output();
+//        l.impvol(americanPut, mD, 4, 100, 000000.1, out );
+//        System.out.println(out.impvol);
 
 
 
